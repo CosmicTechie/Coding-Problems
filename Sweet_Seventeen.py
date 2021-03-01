383447aafa91c10e505354e705b95b6fb60ff78d
Nishant Kumar <62173480+CosmicTechie@users.noreply.github.com>
2021-03-01 21:41:42 +0530

    Update Sweet_Seventeen.py

--- Sweet_Seventeen.py
+++ Sweet_Seventeen.py
@@ -1,3 +1,5 @@
+# Given a maximum of four digit to the base 17 (10 – A, 11 – B, 12 – C, 13 – D … 16 – G} as input, output its decimal value.
+
 '''The int() function converts the specified value into an integer number.
 We are using the same int() method to convert the given input.
 int() accepts two arguments, number and base.
